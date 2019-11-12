494cff0c64316f2d71a2bd5ce86b231884d78fe9
Thomas Humphrey <thumphrey9123@gmail.com>
2019-11-12 17:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   if(string === string.toUpperCase){
-    return 
+    return 'YES INDEED!'
   } else if (string === string.toLowerCase()){
     return 'I can\'t hear you!'
   } else if(string === "I love you, Grandma."){
-    return 
+    return 'I love you too.'
   }
 }
